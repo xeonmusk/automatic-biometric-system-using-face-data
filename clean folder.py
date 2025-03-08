4b94fab213b97dde2d37a71ca375716abf99600a
Vamsi Chintada <vamsi52753@gmail.com>
2025-03-09 01:47:52 +0530

    Update clean folder.py

--- clean folder.py
+++ clean folder.py
@@ -1,22 +1,21 @@
-
 import os
 import shutil
 
 def make_folder_clean(folder_path):
-        if os.path.exists(folder_path):
-            # Remove all files and subdirectories
-            for item in os.listdir(folder_path):
-                item_path = os.path.join(folder_path, item)
-                if os.path.isfile(item_path):
-                    os.remove(item_path)
-                elif os.path.isdir(item_path):
-                    shutil.rmtree(item_path)
-make_folder_clean(r'C:\Users\Lenovo\Desktop\final_project-main\Detected_Objects')
-#make_folder_clean(r'C:\Users\Lenovo\Desktop\final_project-main\datasets\actual_data')
+    if os.path.exists(folder_path):
+        # Remove all files and subdirectories
+        for item in os.listdir(folder_path):
+            item_path = os.path.join(folder_path, item)
+            if os.path.isfile(item_path):
+                os.remove(item_path)
+            elif os.path.isdir(item_path):
+                shutil.rmtree(item_path)
 
-#make_folder_clean(r'C:\Users\Lenovo\Desktop\final_project-main\datasets\test')
-#make_folder_clean(r'C:\Users\Lenovo\Desktop\final_project-main\datasets\val')
-#make_folder_clean(r'C:\Users\Lenovo\Desktop\final_project-main\datasets\testing_data\f')
-#make_folder_clean(r'C:\Users\Lenovo\Desktop\final_project-main\datasets\testing_data\s')
-#make_folder_clean(r'C:\Users\Lenovo\Desktop\final_project-main\datap')
-
+# Updated paths for Codespaces environment
+make_folder_clean('/workspaces/automatic-biometric-system-using-face-data/Detected_Objects')
+#make_folder_clean('/workspaces/automatic-biometric-system-using-face-data/datasets/actual_data')
+#make_folder_clean('/workspaces/automatic-biometric-system-using-face-data/datasets/preprocessed_data/test')
+#make_folder_clean('/workspaces/automatic-biometric-system-using-face-data/datasets/preprocessed_data/val')
+#make_folder_clean('/workspaces/automatic-biometric-system-using-face-data/datasets/testing_data/f')
+#make_folder_clean('/workspaces/automatic-biometric-system-using-face-data/datasets/testing_data/s')
+#make_folder_clean('/workspaces/automatic-biometric-system-using-face-data/datap')
